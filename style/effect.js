7fe910a0d34122f206f3da6e3b505d5293b4d341
FreakLign <stlign@outlook.com>
2023-03-20 20:26:18 +0800

    update

---
+++ style/effect.js
@@ -0,0 +1,7 @@
+setInterval(()=>{
+    var time = new Date();
+    var startTime = new Date("2023/02/14 22:00");
+    var st = time - startTime;
+    document.getElementById("timenow").innerHTML = 
+     "我们相爱了" + parseInt(st / 1000 / 60 / 60 / 24).toString() + "天啦~~";
+},1000);
